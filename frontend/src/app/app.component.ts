a8be89482de332908b507a8dfebed53bc61fd441
Israel <israel.pe@outlook.es>
2024-09-11 10:51:56 -0500

    feat: frontend

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AppHeaderComponent } from "./shared/app-header/app-header.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, AppHeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'frontend';
+}
