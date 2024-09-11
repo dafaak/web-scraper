f4c5606577c626e91956117a137b8dae710be037
Israel <israel.pe@outlook.es>
2024-09-10 19:31:23 -0500

    feat: test hacker news scraper returns a string when fetch is successfull

--- tests/hacker-news-scraper.test.ts
+++ tests/hacker-news-scraper.test.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { HackerNewsScraper } from "../src/models/hacker-news-scraper";
+import { HackerNewsHtmlMock } from "./mocks/hacker-news-html-mock";
+
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -14,6 +16,15 @@ describe('HackerNewsScraper', () => {
   it('Should throw error if fetch fails', async () => {
     mockedAxios.get.mockRejectedValue('');
     await expect(scraper.fetchHtml()).rejects.toThrow('Error fetching HTML');
+  });
+
+  it('should return a string when fetch is succesfull', async () => {
+    const htmlMock = HackerNewsHtmlMock;
+
+    mockedAxios.get.mockResolvedValue({data: htmlMock});
+
+    await expect(scraper.fetchHtml()).resolves.toBe(htmlMock);
+
   });
 
 });
