0445bb6947a66dcac1240263d683fbd9231c8666
Israel <israel.pe@outlook.es>
2024-09-11 11:35:05 -0500

    feat: added cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,10 @@ import express from 'express';
 import { AppRoutes } from "./routes/routes"
 import { envs } from "./config/envs";
 
+var cors = require('cors')
+
 const app = express();
+app.use(cors());
 
 const PORT = envs.port;
 
