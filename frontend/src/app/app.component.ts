a7247bf27fdffbccd06dac1699bc62ddedea6a18
Israel <israel.pe@outlook.es>
2024-09-11 14:14:00 -0500

    feat: news table added to app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppHeaderComponent } from "./shared/app-header/app-header.component";
+import { NewsTableComponent } from "./components/news-table/news-table.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppHeaderComponent],
+  imports: [RouterOutlet, AppHeaderComponent, NewsTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
