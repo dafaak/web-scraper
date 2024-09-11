f3b8e0e2418b81dab17da35a19db39fdc3b2b0c5
Israel <israel.pe@outlook.es>
2024-09-11 14:10:39 -0500

    feat: decode and  delete  symbols from title

--- backend/src/models/hacker-news.ts
+++ backend/src/models/hacker-news.ts
@@ -1,21 +1,28 @@
+import { decodeHtmlEntities } from "../utils/decode-he";
+
 export class HackerNews {
   title: string;
   number: number;
-  points: number;
+  points: number = 0;
   numberOfComments: number = 0;
 
-  constructor(params: { title: string, number: number, points: number, numberOfComments?: number }) {
+  constructor(params: {
+    title: string,
+    number: number,
+    points: number,
+    numberOfComments?: number
+  }, decodeHtmlEntitiesFunction: (htmlString: string) => string = decodeHtmlEntities) {
 
     const {title, number, points, numberOfComments} = params;
 
     if (!title) throw new Error('Title required!');
-    this.title = title;
+    const decodedTitle = decodeHtmlEntitiesFunction(title);
+    this.title = decodedTitle.replace(/[^a-zA-Z0-9\s]/g, '');
 
     if (!number) throw new Error('Number required!');
     this.number = number;
 
-    if (!points) throw new Error('Points required!');
-    this.points = points;
+    if (points) this.points = points;
 
     if (numberOfComments) this.numberOfComments = numberOfComments;
 
